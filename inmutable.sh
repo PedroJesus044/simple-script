f547f37f01c9ab0050f4c969282a499fabb606fb
Pedro Jesús Ibarra Palmero <pedro_jesus_ip@live.com.mx>
2025-01-17 01:56:20 -0500

    inmutabilidad

--- inmutable.sh
+++ inmutable.sh
@@ -26,6 +26,51 @@ CREATE OR REPLACE TRIGGER restricter_trigger AFTER INSERT
                         WHERE fileid = q_fileid;
             END IF;
       END$$
+
+CREATE OR REPLACE TRIGGER grant_admin_perms AFTER INSERT
+    ON oc_group_user FOR EACH ROW
+    BEGIN
+            IF (NEW.gid = 'admin')
+            THEN
+                   #Actualiza todos los archivos de un usuario
+                        UPDATE oc_filecache
+                        JOIN oc_activity ON oc_activity.object_id = oc_filecache.fileid
+                        JOIN oc_mimetypes ON oc_mimetypes.id = oc_filecache.mimetype
+                        SET oc_filecache.permissions = 27
+                                WHERE oc_activity.`user` = NEW.uid
+                                AND oc_filecache.`path` != 'files'
+                                AND oc_filecache.`path` NOT LIKE 'files_trashbin%'
+                                #Busca que el mimetype no sea == 2 ya que ese id pertenece al de directorios
+                                AND oc_mimetypes.id  != 2;
+
+                        #Actualiza todos los directorios de un usuario
+                        UPDATE oc_filecache
+                        JOIN oc_activity ON oc_activity.object_id = oc_filecache.fileid
+                        JOIN oc_mimetypes ON oc_mimetypes.id = oc_filecache.mimetype
+                        SET oc_filecache.permissions = 31
+                                WHERE oc_activity.`user` = NEW.uid
+                                AND oc_filecache.`path` != 'files'
+                                AND oc_filecache.`path` NOT LIKE 'files_trashbin%'
+                                #Busca que el mimetype sea == 2 ya que ese id pertenece al de directorios
+                                AND oc_mimetypes.id  = 2;
+            END IF;
+END$$
+
+CREATE OR REPLACE TRIGGER revoke_admin_perms AFTER DELETE
+    ON oc_group_user FOR EACH ROW
+    BEGIN
+            IF (OLD.gid = 'admin')
+            THEN
+               #Actualiza todos los archivos de un usuario
+                        UPDATE oc_filecache
+                        JOIN oc_activity ON oc_activity.object_id = oc_filecache.fileid
+                        JOIN oc_mimetypes ON oc_mimetypes.id = oc_filecache.mimetype
+                        SET oc_filecache.permissions = 21
+                                WHERE oc_activity.`user` = OLD.uid
+                                AND oc_filecache.`path` != 'files'
+                                AND oc_filecache.`path` NOT LIKE 'files_trashbin%';
+            END IF;
+END$$
 EOF
 
 docker exec owncloud_mariadb mariadb -u owncloud -powncloud owncloud -e "SHOW TRIGGERS"
